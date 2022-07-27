53e26ef27a5b604faa04c9c3eff36a7b1b9f0db9
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-07-27 09:42:52 +0300

    screen setup

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,1 +1,17 @@
-print("Hello World!!")
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
