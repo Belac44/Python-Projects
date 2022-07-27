538fabb2e66efb841432c6d651547b0e1f74263d
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-07-27 12:37:54 +0300

    getting the snake to move around

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,9 +1,39 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+
+
+starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+for pos in starting_pos:
+    new_segment = Turtle("square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto (pos)
+    segments.append(new_segment)
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for i in range(len(segments) - 1, 0, -1):
+        segments[i].goto(segments[i-1].xcor(), segments[i-1].ycor())
+
+
+    segments[0].forward(20)
+    segments[0].left(90)
+
+
+
 
 
 
