f5ce9d37a150ddb738ae3c6d384da469b81d8185
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-07-27 13:40:54 +0300

    Controlling Snake Using keyboard arrows

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 
 starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.setpos()
+        self.head = self.segments[0]
 
     def setpos(self):
         for pos in starting_pos:
@@ -20,5 +26,21 @@ class Snake:
     def move(self):
         for i in range(len(self.segments) - 1, 0, -1):
             self.segments[i].goto(self.segments[i - 1].xcor(), self.segments[i - 1].ycor())
-        self.segments[0].forward(MOVE_DISTANCE)
-        self.segments[0].left(90)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
