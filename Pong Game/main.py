b3cb30add7ab5a447541f1a4cf5e8f2e569ae67b
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-07-28 11:52:43 +0300

    Change turns between Players

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -33,7 +33,11 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and  ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    if ball.xcor() > 395 or ball.xcor() < -395:
-        game_is_on = False
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 screen.exitonclick()
