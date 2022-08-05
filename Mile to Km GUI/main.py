0cab08b5004a62cd8831ca662b3a8fee6c53ee0c
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-08-05 10:47:42 +0300

    First Attempt

--- Mile to Km GUI/main.py
+++ Mile to Km GUI/main.py
@@ -1,9 +1,32 @@
 from tkinter import *
+import math
 
 window = Tk()
-window.minsize(width=200, height=100)
+window.minsize(width=400, height=300)
 window.title("Miles to Km Converter")
+window.config(padx=100, pady=100)
+
+inputs = Entry(width=10)
+inputs.grid(column=3, row=1)
+
+miles = Label(text="Miles")
+miles.grid(column=4, row=1)
+
+equal = Label(text="is equal to")
+equal.grid(column=2, row=2)
+
+converted = Label(text="")
+converted.grid(column=3, row=2)
+
+km = Label(text="Km")
+km.grid(column=4, row=2)
 
 
+def convert():
+    converted['text'] = math.ceil(int(inputs.get()) * 1.6)
+
+
+button = Button(text="calculate", command=convert)
+button.grid(column=3, row=3)
 
 window.mainloop()
