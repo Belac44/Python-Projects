67037da218d3c59ad109c8f571d3d4ef5b2ca16e
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-08-06 18:32:09 +0300

    Capture Email and Password

--- Password Manger/main.py
+++ Password Manger/main.py
@@ -3,40 +3,51 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save_credentials():
+    web = web_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as file:
+        text = f"{web} | {email} | {password}"
+        file.write(f"{text}\n")
+        web_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+#Canvas
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file = "logo.png")
+logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-
+#Labels
 website = Label(text="Website:")
 website.grid(column=0, row=1)
+emails = Label(text="Email/Username:")
+emails.grid(column=0, row=2)
+passwords = Label(text="Password:")
+passwords.grid(column=0, row=3)
 
+#Entries
 web_entry = Entry(width=54)
 web_entry.grid(column=1, row=1, columnspan=2)
-
-email = Label(text="Email/Username:")
-email.grid(column=0, row=2)
-
+web_entry.focus()
 email_entry = Entry(width=54)
 email_entry.grid(column=1, row=2, columnspan=2)
-
-password = Label(text="Password:")
-password.grid(column=0, row=3)
-
+email_entry.insert(0, "calebbosire44@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(column=1, row=3)
 
-
+#Buttons
 generate = Button(text="Generate Password")
 generate.grid(column=2, row=3)
-
-add = Button(text="add", width=45)
+add = Button(text="add", width=45, command=save_credentials)
 add.grid(column=1, row=4, columnspan=2)
 
 
