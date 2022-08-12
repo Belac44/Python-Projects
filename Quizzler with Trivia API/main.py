e0035480964a6d7d7415f8fb8a4fd4d6180eb797
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-08-12 17:39:09 +0300

    UI Setup

--- Quizzler with Trivia API/main.py
+++ Quizzler with Trivia API/main.py
@@ -1,7 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -13,8 +13,10 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface()
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
