b9a405d0553fbbe8a05d64f8e6af74c3b1ff9ec9
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-08-14 21:07:46 +0300

    Adding a pixel to the graph

--- Habit Tracking/main.py
+++ Habit Tracking/main.py
@@ -3,10 +3,11 @@ import requests
 pixela_endpoint = "https://pixe.la/v1/users"
 TOKEN = "hbxiahyyi0bjbcbjajd"
 USERNAME = "belac"
+GRAPH_ID = "codegraph"
 user_params = {
     "token": TOKEN,
     "username":USERNAME,
-    "agreeTermsOfService":"yes",
+    "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
@@ -16,7 +17,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "codegraph",
+    "id": GRAPH_ID,
     "name": "Coding graph",
     "unit": "hours",
     "type": "float",
@@ -26,5 +27,15 @@ graph_config = {
 headers = {
     "X-USER-TOKEN":TOKEN
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+graph_data = {
+    "date": "20220814",
+    "quantity": "2.1",
+
+
+}
+response = requests.post(url=pixel_creation_endpoint, json=graph_data, headers=headers)
 print(response.text)
