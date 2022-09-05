eba3814c466fb61d95573a613e9e5fabd0d3a7f6
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-09-05 18:27:00 +0300

    Functionality without a database

--- Databases/main.py
+++ Databases/main.py
@@ -4,17 +4,24 @@ app = Flask(__name__)
 
 all_books = []
 
-
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', books=all_books, length=len(all_books))
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form['book'],
+            "author": request.form['author'],
+            "rating": request.form['rating'],
+        }
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
 
