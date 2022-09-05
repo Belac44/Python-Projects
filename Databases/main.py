d081b3a552aaa5d612db033a358f8f60c3aa272c
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-09-05 22:05:43 +0300

    Creating Database directly using sqlite3

--- Databases/main.py
+++ Databases/main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
 all_books = []
+
+# db = sqlite3.connect("book-collection.db")
+# cursor = db.cursor()
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
+# #                " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
 @app.route('/')
 def home():
