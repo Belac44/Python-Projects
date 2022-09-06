0715cb49d758f152afc5979d6f035e9102f017c0
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-09-06 09:16:48 +0300

    Delete Edit and get books from database

--- Databases/main.py
+++ Databases/main.py
@@ -1,34 +1,63 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-all_books = []
 
-# db = sqlite3.connect("book-collection.db")
-# cursor = db.cursor()
-# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
-# #                " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-#
-# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-# db.commit()
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books, length=len(all_books))
+    return render_template('index.html', books=db.session.query(Book).all())
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form['book'],
-            "author": request.form['author'],
-            "rating": request.form['rating'],
-        }
-        all_books.append(new_book)
+        new_book = Book(title=request.form['book'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
+
+@app.route('/edit/<int:book_id>', methods=['GET', 'POST'])
+def edit(book_id):
+
+    if request.method == "POST":
+        book_to_update = Book.query.get(book_id)
+
+        book_to_update.title = request.form['book']
+        book_to_update.author = request.form['author']
+        book_to_update.rating = request.form['rating']
+        try:
+            db.session.commit()
+        except IntegrityError:
+            pass
+        return redirect(url_for('home'))
+    return render_template("edit.html", id=book_id, book=Book.query.get(book_id))
+
+@app.route("/delete/<int:book_id>")
+def delete(book_id):
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        pass
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
